a88379e21001ee98a51b4ecccbb2c9f32f510dbd
this-isaryan <aryankumar11@outlook.com>
2026-01-19 16:17:36 -0500

    Optimize Model Loading & Caching

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from ai_pipeline.model_loader import load_model
 from backend.conn_db import engine, Base
 from backend.routers import vendor as vendor_router
 from backend.routers import invoice as invoice_router
@@ -8,7 +9,9 @@ app = FastAPI(title="VeriPay API")
 
 from backend.routers.invoice_analysis import router as invoice_analysis_router
 
-app.include_router(invoice_analysis_router)
+@app.on_event("startup")
+def startup_event():
+    load_model()
 
 # CORS (required for frontend)
 app.add_middleware(
