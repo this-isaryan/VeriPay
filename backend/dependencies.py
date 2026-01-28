25a866e4df653bbfd95a44d583ce8111c4b90395
Aryan <164097681+this-isaryan@users.noreply.github.com>
2026-01-28 15:41:39 -0500

    Delete backend directory

--- backend/dependencies.py
+++
@@ -1,8 +0,0 @@
-from backend.conn_db import SessionLocal
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- backend/routers/predict.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from ai_pipeline.inference import run_inference
-
-router = APIRouter()
-
-@router.post("/predict")
-def predict(text: str):
-    result = run_inference(text)
-    return {"prediction": result}
